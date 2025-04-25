8e177a200be23cdd7d16f6abc9c144efa6a98519
i_oosera <i_oosera@LAPTOP-3CFBO0CG>
2025-04-25 15:04:20 +0900

    a

--- src/scoremanager/main/LogoutAction.java
+++ src/scoremanager/main/LogoutAction.java
@@ -1,5 +1,17 @@
 package scoremanager.main;
 
-public class LogoutAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import tool.Action;
+
+public class LogoutAction extends Action {
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // セッションを無効化
+        request.getSession().invalidate();
+
+        // ログアウト画面へフォワード
+        return "scoremanager/main/logout.jsp";
+    }
 }
